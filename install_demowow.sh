8a4b2d1f6ac403595c2dbfef547ed42120e67b46
EC2 Default User <ec2-user@ip-172-31-44-203.ec2.internal>
2022-04-20 03:08:38 +0000

    Update install_demowow.sh for release 1.6.4

--- install_demowow.sh
+++ install_demowow.sh
@@ -47,11 +47,11 @@ sudo yum --enablerepo=epel install ncdu -y
 sudo /home/ec2-user/install_wetty.sh "$1"
 
 #Get latest Demowow Image
-wget -O demowow.zip https://github.com/jgmsteinfeld/demowow/releases/download/v1.6.3/demowow-1.6.3.zip 
+wget -O demowow.zip https://github.com/jgmsteinfeld/demowow/releases/download/v1.6.4/demowow-1.6.4.zip 
 unzip demowow.zip
 rm -f demowow.zip
-mv demowow-1.6.3/* .
-rm -rf demowow-1.6.3
+mv demowow-1.6.4/* .
+rm -rf demowow-1.6.4
 
 #Setup Crontab Entries to keep disk clean
 (sudo crontab -l 2>/dev/null; echo "*30 * * * * /home/ec2-user/clear_logs.sh") | sudo crontab -
