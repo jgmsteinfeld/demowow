7920b280c6c8169c84ca803065ebeec7edf1004b
EC2 Default User <ec2-user@ip-172-31-44-203.ec2.internal>
2022-04-25 02:09:05 +0000

    update install_falcon_agent.sh

--- install_falcon_agent.sh
+++ install_falcon_agent.sh
@@ -5,7 +5,7 @@ if [ $rhel == 7 ]; then
    wget -O falcon-sensor-6.37.0-13402.el7.x86_64.rpm https://surf.service-now.com/sys_attachment.do?sys_id=da090311db664910b0b75ac2ca96197a 
 fi
 
-sudo yum install ./falcon-sensor-6.28.0-12504.el7.x86_64\(6.28.12504i\).rpm -y
+sudo yum install ./falcon-sensor-6.37.0-13402.el7.x86_64.rpm -y
 sleep 5
 sudo /opt/CrowdStrike/falconctl -s -f --cid=77096FEA2CDA4F6E9B6D3604E826949F-E1
 sleep 2
