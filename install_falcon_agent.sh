8c0e231f3cf9a50e03818fc21aa015918ef46fd4
EC2 Default User <ec2-user@ip-172-31-44-203.ec2.internal>
2022-04-25 02:51:46 +0000

    updates to install_demowow.sh

--- install_falcon_agent.sh
+++ install_falcon_agent.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-falcon-sensor-6.37.0-13402.el7.x86_64.rpm
+fs = falcon-sensor-6.37.0-13402.el7.x86_64.rpm
 rhel=`rpm -E %{rhel}`
 if [ $rhel == 7 ]; then
-   wget -O falcon-sensor-6.37.0-13402.el7.x86_64.rpm https://surf.service-now.com/sys_attachment.do?sys_id=da090311db664910b0b75ac2ca96197a 
+   wget -O $fs https://surf.service-now.com/sys_attachment.do?sys_id=da090311db664910b0b75ac2ca96197a 
 fi
 
-sudo yum install ./falcon-sensor-6.37.0-13402.el7.x86_64.rpm -y
+sudo yum install ./$fs -y
 sleep 5
 sudo /opt/CrowdStrike/falconctl -s -f --cid=77096FEA2CDA4F6E9B6D3604E826949F-E1
 sleep 2
@@ -13,4 +13,4 @@ sudo service falcon-sensor start
 sleep 5
 ps -e | grep falcon-sensor
 sudo service falcon-sensor status | grep connected
-rm falcon-sensor-6.28.0-12504.el7.x86_64\(6.28.12504i\).rpm
+rm $fs 
